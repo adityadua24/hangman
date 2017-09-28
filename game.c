97bf1ef9921a490b1f6326cb712e0712ab2fec93
Aditya Dua <adityadua24@gmail.com>
2017-09-28 14:19:13 +1000

    Game interface counts fixed

--- game.c
+++ game.c
@@ -140,18 +140,8 @@ int start_playing(int *connfd, session_info *this_session){
     int num_guess = min(strlen(pair)-1+10, 26); // strlen(pair)-1 to remove space between words 
     char *got_right = form_got_right(pair);
     char *guessed = (char *)malloc(sizeof(char) * num_guess);
-    if ((game_ui(connfd, guessed, &num_guess, got_right, 0)) == 0){
-        return 0;
-    }
-    while(num_guess > 0){
-        char *guess = read_segment(connfd);
-        if (guess == NULL){
-            return 0;
-        }
-        num_guess--;
-        match_guess_pair(guess, pair, got_right);
-        guessed = strcat(guessed, guess);
-        if((strchr(got_right, '_')) == NULL){
+    while(num_guess >= 0){
+        if(((strchr(got_right, '_')) == NULL) || num_guess == 0) {
             if ((game_ui(connfd, guessed, &num_guess, got_right, 1)) == 0){
                 return 0;
             }
@@ -162,6 +152,13 @@ int start_playing(int *connfd, session_info *this_session){
                 return 0;
             }
         }
+        char *guess = read_segment(connfd);
+        if (guess == NULL){
+            return 0;
+        }
+        num_guess--;
+        match_guess_pair(guess, pair, got_right);
+        guessed = strcat(guessed, guess);
     }
     if ((game_over(connfd, num_guess, this_session)) == 0){
         return 0;
@@ -203,9 +200,9 @@ long random_at_most(long max) {
   
     // Truncated division is intentional
     return x/bin_size;
-  }
+}
 
-  int game_ui(int *connfd, char *guessed, int *num_guess, char *got_right, int last){
+int game_ui(int *connfd, char *guessed, int *num_guess, char *got_right, int last){
     char *ui;
     ui = (char *)malloc(sizeof(char)*256);
     char *head = "\n------------------------------------\n";
@@ -222,7 +219,7 @@ long random_at_most(long max) {
     ui = strcat(ui, line3);
     ui = strcat(ui, got_right);
     char *line4 = "\n\nEnter your guess - ";
-    if(last != 1){
+    if(last == 0){
         ui = strcat(ui, line4);
         if ((send_segment(connfd, ui, strlen(ui))) == 0){
             return 0;
@@ -232,7 +229,7 @@ long random_at_most(long max) {
             return 0;
         }
     }
-    else{
+    else if(last == 1){
         if((send_segment(connfd, ui, strlen(ui))) == 0){
             return 0;
         }
@@ -240,16 +237,16 @@ long random_at_most(long max) {
     return 1;
 }
 
-  int min(int x, int y){
-      if (x >= y){
-          return y;
-      }
-      else {
-          return x;
-      }
-  }
+int min(int x, int y){
+    if (x >= y){
+        return y;
+    }
+    else {
+        return x;
+    }
+}
 
-  char* form_got_right(char *pair){
+char* form_got_right(char *pair){
     char *got_right = (char *)malloc(sizeof(char)*2*strlen(pair)+1); // +1 for string terminator
     *(got_right + 2*strlen(pair)) = '\0';
     char *encodeChar = "_ ";
@@ -263,17 +260,17 @@ long random_at_most(long max) {
         }
     }
     return got_right;
-  }
+}
 
-  void match_guess_pair(char *guess, char *pair, char *got_right){
-      for(int i=0; i < strlen(pair); i++){
-          if (*(pair+i) == *guess){
-              *(got_right+i*2) = *guess;
-          }
-      }
-  }
+void match_guess_pair(char *guess, char *pair, char *got_right){
+    for(int i=0; i < strlen(pair); i++){
+        if (*(pair+i) == *guess){
+            *(got_right+i*2) = *guess;
+        }
+    }
+}
 
-  int game_over(int *connfd, int status, session_info *this_session){
+int game_over(int *connfd, int status, session_info *this_session){
     char *msg = (char *)malloc(sizeof(char)*256);
     if (status > 0){
         char *seg = "\n\nGame Over!\n\nWell done ";
@@ -286,7 +283,7 @@ long random_at_most(long max) {
         }
     }
     else{
-        char *seg = "\nGame Over!\nBad luck ";
+        char *seg = "\n\nGame Over!\nBad luck ";
         msg = strcat(msg, seg);
         msg = strcat(msg, this_session->user);
         seg = "! You have run out of guesses. The Hangman got you!";
@@ -298,11 +295,11 @@ long random_at_most(long max) {
         }
     }
     return 1;
-  }
+}
 
-  void update_session_info(session_info *this_session, char *name, char *pswrd){
+void update_session_info(session_info *this_session, char *name, char *pswrd){
       this_session->user = (char *)malloc(strlen(name));
       this_session->password = (char *)malloc(strlen(pswrd));
       strcpy(this_session->user, name);
       strcpy(this_session->password, pswrd);
-  }
+}
